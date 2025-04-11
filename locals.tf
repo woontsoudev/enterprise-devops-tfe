7d9a56494a6eb49a687540cf9e686e4cd088b65b
Woon Tsou <woontsou@Woons-MacBook-Pro.local>
2025-04-10 20:23:56 -0600

    feature: added workspaces

--- locals.tf
+++ locals.tf
@@ -19,5 +19,61 @@ locals {
       project_id = module.project["enterprise-devops-project"].id
       vcs_repo_identifier = "${var.github_organization_name}/enterprise-devops-github"
     }
+
+    "enterprise-devops-aws-network" = {
+      description         = "Automation for AWS network resources."
+      execution_mode      = "remote"
+      project_id          = module.project["enterprise-devops-project"].id
+      vcs_repo_identifier = "${var.github_organization_name}/enterprise-devops-terraform-aws-network"
+
+      variables = [
+        {
+          category = "terraform"
+          hcl      = true
+          key      = "azs"
+          value    = jsonencode(["us-west-2a", "us-west-2b"])
+        },
+        {
+          category = "terraform"
+          key      = "cidr"
+          value    = "10.0.0.0/16"
+        },
+        {
+          category = "terraform"
+          key      = "name"
+          value    = "enterprise-devops"
+        },
+      ]
+    }
+
+    "enterprise-devops-aws-cluster-prod" = {
+      description         = "Automation for AWS cluster resources."
+      execution_mode      = "remote"
+      project_id          = module.project["enterprise-devops-project"].id
+      vcs_repo_identifier = "${var.github_organization_name}/enterprise-devops-terraform-aws-cluster"
+
+      variables = [
+        {
+          category = "terraform"
+          key      = "domain"
+          value    = "woontsou.com"
+        },
+        {
+          category = "terraform"
+          key      = "environment"
+          value    = "prod"
+        },
+        {
+          category = "terraform"
+          key      = "name"
+          value    = "enterprise-devops-woontsou"
+        },
+        {
+          category = "terraform"
+          key      = "vpc_name"
+          value    = "enterprise-devops"
+        },
+      ]
+    }
   }
 }
